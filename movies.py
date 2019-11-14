debf9fadbea17c36402c9f17758c80e48658430a
pat <patrickhein@posteo.de>
2019-11-14 09:56:44 +0100

    Loading more files

--- movies.py
+++ movies.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 
-movies = pd. read_csv('the-movies-dataset/movies_metadata.csv', low_memory=False)
-movies.head()
+metadata = pd.read_csv('the-movies-dataset/movies_metadata.csv', low_memory=False)
+metadata.head()
+
+keywords = pd.read_csv("the-movies-dataset/keywords.csv", low_memory=False)
+keywords.head()
+
+ratings = pd.read_csv("the-movies-dataset/ratings.csv", low_memory=False)
+ratings.head()
