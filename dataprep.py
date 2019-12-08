c34b9f5436cf07fb9d575675ed7108cbefe7622d
ilaydagurbak <ilaydagurbak@gmail.com>
2019-12-08 15:40:50 +0100

    training model

--- dataprep.py
+++ dataprep.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 # Importing the dataset
 dataset = pd.read_csv('the-movies-dataset/movies_metadata.csv', low_memory=False)
 dataset.head()
@@ -12,22 +11,21 @@ keywords.head()
 ratings = pd.read_csv("the-movies-dataset/ratings.csv", low_memory=False)
 ratings.head()
 
-dataset['budget']=dataset['budget'].replace(0,dataset['budget'].mean())
+dataset['budget'] = dataset['budget'].replace(0, dataset['budget'].mean())
 
 X = dataset.iloc[:, :].values
 y_revenue = dataset.iloc[:, 12].values
 y_rating = dataset.iloc[:, 18].values
 
-
 # picking independent variables
-X = X[:,[0,1,4,9,11,13,14,15,22,23]]
+X = X[:, [0, 1, 4, 9, 11, 13, 14, 15, 22, 23]]
 
 # Removing zero REVENUES from the data
 y_revenue_removed = []
 y_rating_removed = []
 X_removed = []
-for l in range(0,len(y_revenue)):
-    if y_revenue[l] !=0:
+for l in range(0, len(y_revenue)):
+    if y_revenue[l] != 0:
         y_revenue_removed.append(y_revenue[l])
         y_rating_removed.append(y_rating[l])
         X_removed.append(X[l])
@@ -38,13 +36,12 @@ X = np.array(X_removed)
 # Adjusting inflation to 2019 at average inflation - 3.22% do this only if using revenue (12 y index)
 avg_inflation = 1.01322
 year_now = 2019
-for l in range(0,len(y_revenue)):
+for l in range(0, len(y_revenue)):
     try:
-        film_year = int(X[l,4][0:4])
-        y_revenue[l] = y_revenue[l]*(avg_inflation ** (year_now-film_year))
-        X[l,7] = int(film_year)
+        film_year = int(X[l, 4][0:4])
+        y_revenue[l] = y_revenue[l] * (avg_inflation ** (year_now - film_year))
+        X[l, 7] = int(film_year)
     except:
-        X[l,4] = 0
+        X[l, 4] = 0
 
 dataset = pd.DataFrame(X)
-
--- movies.py
+++ movies.py
@@ -1,11 +1,74 @@
 import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
 
-metadata = pd.read_csv('the-movies-dataset/movies_metadata.csv', low_memory=False)
-metadata.head()
-
-keywords = pd.read_csv("the-movies-dataset/keywords.csv", low_memory=False)
-keywords.head()
+dataset = pd.read_csv('the-movies-dataset/movies_metadata.csv', low_memory=False)
+print(dataset.head())
+print(dataset.info())
+# print(dataset.describe())
 
 ratings = pd.read_csv("the-movies-dataset/ratings.csv", low_memory=False)
-ratings.head()
+# print(ratings.head())
+# print(ratings.info())
+
+# After discussing the structure of the data and any problems that need to be
+# Cleaned, perform those cleaning steps in the second part of this section.
+# Drop extraneous columns
+col = ['adult', 'belongs_to_collection', 'homepage', 'original_language', 'overview', 'popularity', 'poster_path',
+       'runtime', 'spoken_languages', 'status', 'tagline', 'title', 'video', 'production_countries']
+dataset.drop(col, axis=1, inplace=True)
+print(dataset.info())
+
+# Drop the duplicates
+dataset.drop_duplicates(inplace=True)
+ratings.drop_duplicates(inplace=True)
+
+# Drop null values from datasets
+col2 = ['budget', 'genres', 'id', 'original_title', 'imdb_id', 'production_companies', 'release_date', 'revenue',
+        'vote_average']
+dataset.dropna(subset=col2, how='any', inplace=True)
+
+print(dataset.isnull().sum())
+
+print(dataset.head(2))
+
+groupedby_movieName = dataset.groupby('original_title')
+# print movie names
+movies = dataset.groupby('original_title').size().sort_values(ascending=True)[:100]
+print(movies)
+
+Solace_data = groupedby_movieName.get_group('Solace')
+print(Solace_data.shape)
+
+# Find and visualize the user votes of the movie “Solace”
+plt.figure(figsize=(10, 10))
+plt.scatter(Solace_data['original_title'], Solace_data['vote_average'])
+plt.title('Plot showing  the user rating of the movie “Solace_data”')
+plt.show()
+
+# Perform machine learning on first 500 extracted records
+
+first_500 = dataset[500, :]
+first_500.dropna(inplace=True)
+
+# Use the following features
+features = first_500[
+    ['budget', 'genres', 'id', 'original_title', 'imdb_id', 'production_companies', 'release_date', 'revenue']].values
+
+# Use votes as label
+labels = first_500[['vote_average']].values
+
+# Create train and test data set
+train, test, train_labels, test_labels = train_test_split(features, labels, test_size=0.33, random_state=42)
+
+# svm training
+svc = SVC()
+svc.fit(train, train_labels)
+Y_pred = svc.predict(test)
+acc_svc = round(svc.score(train, train_labels) * 100, 2)
+acc_svc
