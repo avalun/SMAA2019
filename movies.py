30726277efd419b47557c03292a15608cdcc1e01
ilaydagurbak <ilaydagurbak@gmail.com>
2019-12-11 22:09:44 +0100

    grid search algorithm for svr

--- movies.py
+++ movies.py
@@ -1,23 +1,24 @@
 import pickle
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn import preprocessing, metrics
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 
 
 def prepare_dataset():
     dataset = pd.read_csv('the-movies-dataset/movies_metadata.csv', low_memory=False)
-    print(dataset.head())
-    print(dataset.info())
-    print(dataset.describe())
+    # print(dataset.head())
+    # print(dataset.info())
+    # print(dataset.describe())
 
     ratings = pd.read_csv("the-movies-dataset/ratings.csv", low_memory=False)
-    print(ratings.head())
-    print(ratings.info())
+    # print(ratings.head())
+    # print(ratings.info())
 
     # After discussing the structure of the data and any problems that need to be
     # Cleaned, perform those cleaning steps in the second part of this section.
@@ -46,13 +47,13 @@ def prepare_dataset():
     movies = dataset.groupby('original_title').size().sort_values(ascending=True)[:100]
     print(movies)
 
-    Solace_data = groupedby_movieName.get_group('Solace')
-    print(Solace_data.shape)
+    solace_data = groupedby_movieName.get_group('Solace')
+    print(solace_data.shape)
 
     # Find and visualize the user votes of the movie “Solace”
     plt.figure(figsize=(10, 10))
-    plt.scatter(Solace_data['original_title'], Solace_data['vote_average'])
-    plt.title('Plot showing  the user rating of the movie “Solace_data”')
+    plt.scatter(solace_data['original_title'], solace_data['vote_average'])
+    plt.title('Plot showing the user rating of the movie “Solace_data”')
     plt.show()
 
     le = preprocessing.LabelEncoder()
@@ -72,23 +73,23 @@ def load(filename='dump.p'):
 
 def svm(train, test, train_labels, test_labels):
     # Svm training
-    svc = SVR(kernel="rbf", C=1e3, gamma=1e-8, epsilon=0.1)
-    # svc = SVR(kernel="rbf", C=10000, gamma=1e-8, epsilon=0.1)
+    svc = GridSearchCV(SVR(kernel='rbf', gamma=[1e-8, 1e-7, 100]), cv=5,
+                       param_grid={"C": [1e0, 1e1, 1e2, 1e3],
+                                   "gamma": np.logspace(-2, 2, 5)})
 
     svc.fit(train, train_labels)
-    Y_pred = svc.predict(test)
-    print(Y_pred)
+    y_pred = svc.predict(test)
+    print(y_pred)
     print(test_labels)
-    print('SVR - Mean Absolute Error:', metrics.mean_absolute_error(test_labels, Y_pred))
-    print('SVR - Mean Squared Error:', metrics.mean_squared_error(test_labels, Y_pred))
-    print('SVR - Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_labels, Y_pred)))
+    print('SVR - Mean Absolute Error:', metrics.mean_absolute_error(test_labels, y_pred))
+    print('SVR - Mean Squared Error:', metrics.mean_squared_error(test_labels, y_pred))
+    print('SVR - Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_labels, y_pred)))
 
 
-def random_forest(train, test, train_labels, test_labels, n_estimators=1000):
+def random_forest(train, test, train_labels, test_labels, n_estimators=2000):
     regressor = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
     regressor.fit(train, train_labels)
     y_pred = regressor.predict(test)
-    regressor.score(train, train_labels)
     acc_random_forest = round(regressor.score(train, train_labels) * 100, 2)
     print("Random Forest", n_estimators, "Estimators - Accuracy", acc_random_forest)
     print("Random Forest", n_estimators, "Estimators - Mean Absolute Error:",
@@ -104,18 +105,17 @@ def main():
     prepare_dataset()
     # dump(dataset)
     dataset = load()
-    x = dataset.iloc[0:500, :-1].values
-    y = dataset.iloc[0:500, -1].values
+    x = dataset.iloc[0:1000, :-1].values
+    y = dataset.iloc[0:1000, -1].values
     train, test, train_labels, test_labels = train_test_split(x, y, test_size=0.33, random_state=25)
     svm(train, test, train_labels, test_labels)
 
     # Random Forests with different numbers of estimators
     # Larger number of n_estimators become useful for bigger datasets
     # so enable the lower lines for the final testing!
-    random_forest(train, test, train_labels, test_labels, 500)
-    # random_forest(train, test, train_labels, test_labels, 1000)
-    # andom_forest(train, test, train_labels, test_labels, 2000)
+    random_forest(train, test, train_labels, test_labels)
     # random_forest(train, test, train_labels, test_labels, 4000)
+    # random_forest(train, test, train_labels, test_labels, 8000)
 
 
 if __name__ == '__main__':
