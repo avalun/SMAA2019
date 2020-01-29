fbb5185812317fb3d0ea37ff7124b16cb50feff2
ilaydagurbak <ilaydagurbak@gmail.com>
2020-01-29 15:25:35 +0100

    Merge branch 'master' of /Users/ilayda/PycharmProjects/SMAA with conflicts.

--- movies.py
+++ movies.py
@@ -9,7 +9,6 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
-from sklearn.metrics import r2_score
 
 
 def prepare_dataset():
@@ -21,10 +20,12 @@ def prepare_dataset():
     ratings = pd.read_csv("the-movies-dataset/ratings.csv", low_memory=False)
     # print(ratings.head())
     # print(ratings.info())
-    plt.figure(figsize=(18,8))
-    sns.barplot(x='original_title', y='vote_average', data=dataset.head(10))
-    plt.show()
+    # plt.figure(figsize=(30,8))
+    # sns.scatterplot(x='id', y='vote_average', data=dataset.head(100))
+    # plt.show()
 
+    # After discussing the structure of the data and any problems that need to be
+    # Cleaned, perform those cleaning steps in the second part of this section.
     # Drop extraneous columns
     col = ['adult', 'belongs_to_collection', 'homepage', 'original_language', 'overview', 'popularity', 'poster_path',
            'runtime', 'spoken_languages', 'status', 'tagline', 'title', 'video', 'production_countries', 'vote_count',
@@ -41,17 +42,17 @@ def prepare_dataset():
             'vote_average']
     dataset.dropna(subset=col2, how='any', inplace=True)
 
-    #print(dataset.isnull().sum())
+    print(dataset.isnull().sum())
 
-    #print(dataset.head(2))
+    print(dataset.head(2))
 
     groupedby_movieName = dataset.groupby('original_title')
     # print movie names
     movies = dataset.groupby('original_title').size().sort_values(ascending=True)[:100]
-    #print(movies)
+    print(movies)
 
     solace_data = groupedby_movieName.get_group('Solace')
-    #print(solace_data.shape)
+    print(solace_data.shape)
 
     # Find and visualize the user votes of the movie “Solace”
     plt.figure(figsize=(10, 10))
@@ -78,8 +79,8 @@ def svm(train, test, train_labels, test_labels):
     # Svm training
     parameters = {
         "kernel": ["rbf"],
-        "C": [1,10,10,100,1000],
-        "gamma": [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+        "C": [1,10,100,1000],
+        "gamma": [1e-8, 1e-7, 1e-6, 1e-5]
     }
 
     grid = GridSearchCV(SVR(), parameters, cv=5, verbose=2)
@@ -87,48 +88,56 @@ def svm(train, test, train_labels, test_labels):
 
     #svc.fit(train, train_labels)
     y_pred = grid.predict(test)
-    r2 = r2_score(test_labels, y_pred)
-    #print(y_pred)
-    #print(test_labels)
+    print(y_pred)
+    print(test_labels)
     print('SVR - Mean Absolute Error:', metrics.mean_absolute_error(test_labels, y_pred))
     print('SVR - Mean Squared Error:', metrics.mean_squared_error(test_labels, y_pred))
     print('SVR - Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_labels, y_pred)))
-    print('SVR - R2 metric:', r2)
 
 
 def random_forest(train, test, train_labels, test_labels, n_estimators=2000):
     regressor = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
     regressor.fit(train, train_labels)
     y_pred = regressor.predict(test)
-
-    r2 = r2_score(test_labels, y_pred)
+    acc_random_forest = round(regressor.score(train, train_labels) * 100, 2)
+    print("Random Forest", n_estimators, "Estimators - Accuracy", acc_random_forest)
     print("Random Forest", n_estimators, "Estimators - Mean Absolute Error:",
           metrics.mean_absolute_error(test_labels, y_pred))
     print("Random Forest", n_estimators, "Estimators - Mean Squared Error:",
           metrics.mean_squared_error(test_labels, y_pred))
     print("Random Forest", n_estimators, "Estimators - Root Mean Squared Error:",
           np.sqrt(metrics.mean_squared_error(test_labels, y_pred)))
-    print('Random Forest - R2 metric:', r2)
 
 
 def main():
     # Create train and test data set
-    # dataset = prepare_dataset()
+    #prepare_dataset()
     # dump(dataset)
-    dataset = load()
-    print(dataset.head())
-    x = dataset.iloc[0:3000, :-1].values
-    y = dataset.iloc[0:3000, -1].values
-    train, test, train_labels, test_labels = train_test_split(x, y, test_size=0.33, random_state=42)
-    svm(train, test, train_labels, test_labels)
-    print(test_labels)
+    #dataset = load()
+    #x = dataset.iloc[:, :-1].values
+    #y = dataset.iloc[:, -1].values
+    #train, test, train_labels, test_labels = train_test_split(x, y, test_size=0.33, random_state=42)
+    #svm(train, test, train_labels, test_labels)
 
     # Random Forests with different numbers of estimators
     # Larger number of n_estimators become useful for bigger datasets
     # so enable the lower lines for the final testing!
-    random_forest(train, test, train_labels, test_labels)
+    #random_forest(train, test, train_labels, test_labels)
     # random_forest(train, test, train_labels, test_labels, 4000)
     # random_forest(train, test, train_labels, test_labels, 8000)
+    dataset = load()
+    x = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
+    train, test, train_labels, test_labels = train_test_split(x, y, test_size=0.33, random_state=42)
+    grid = load('grid.p')
+    y_pred = grid.predict(test)
+    print(y_pred)
+    print(test_labels)
+    print('SVR - Mean Absolute Error:', metrics.mean_absolute_error(test_labels, y_pred))
+    print('SVR - Mean Squared Error:', metrics.mean_squared_error(test_labels, y_pred))
+    print('SVR - Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_labels, y_pred)))
+    print('SVR - R Squared Error:', metrics.r2_score(test_labels, y_pred))
+    print(grid.best_params_)
 
 
 if __name__ == '__main__':
